5c476495635f7a1277920b91d03092cee20c80b9
Precious Diala <dialpressy@gmail.com>
2022-09-07 21:46:58 +0000

    Update the builtins_help_2 file

--- builtins_help_2.c
+++ builtins_help_2.c
@@ -6,7 +6,7 @@ void help_unsetenv(void);
 void help_history(void);
 
 /**
- * help_env - Displays information on the shellby builtin command 'env'.
+ * help_env - Displays information on the shell by builtin command 'env'.
  */
 void help_env(void)
 {
@@ -16,7 +16,7 @@ void help_env(void)
 }
 
 /**
- * help_setenv - Displays information on the shellby builtin command 'setenv'.
+ * help_setenv - Displays information on the shell by builtin command 'setenv'.
  */
 void help_setenv(void)
 {
@@ -30,7 +30,7 @@ void help_setenv(void)
 }
 
 /**
- * help_unsetenv - Displays information on the shellby builtin command
+ * help_unsetenv - Displays information on the shell by builtin command
  * 'unsetenv'.
  */
 void help_unsetenv(void)
