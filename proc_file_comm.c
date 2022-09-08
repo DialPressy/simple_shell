aa74dc8a300d3c8ea3a7c154bc9db8eb7b6f3654
Precious Diala <dialpressy@gmail.com>
2022-09-08 00:57:30 +0000

    Update proc_file_comm.c

--- proc_file_comm.c
+++ proc_file_comm.c
@@ -8,7 +8,7 @@ int proc_file_commands(char *file_path, int *exe_ret);
  * a cant open error.
  * @file_path: Path to the supposed file.
  *
- * Return: 127.
+ * Return: 127
  */
 
 int cant_open(char *file_path)
